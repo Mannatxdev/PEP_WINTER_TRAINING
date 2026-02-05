7f78a0530fa03471ea5276838406a3250d110ae1
{manna} <cybertyist@gmail.com>
2026-02-05 11:22:43 +0530

    curd created

--- project1/polls/models.py
+++ project1/polls/models.py
@@ -1,12 +1,26 @@
+
 from django.db import models
 
 # Create your models here.
-class user:
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    # address = models.CharField(max_length=100)
+class user(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    # address  = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    # phone_num = models.CharField(max_length=10)
+    # phone_number = models.CharField(max_length=15)
 
     def __str__(self):
-        return f"{self.first_name}{self.last_name} < {self.email} > "
+        return f"{self.first_name} {self.last_name} < {self.email} >"
+    
+
+    class FormModel(models.Model):
+        title = models.CharField(max_length=200)
+        description = models.TextField()
+        last_modified = models.DateTimeField(auto_now_add = True)
+        img = models.ImageField(upload_to= "images/")
+
+
+        # renames the instances of  the model
+        # with their title name
+    def __str__(self):
+        return self.title
--- project1/polls/views.py
+++ project1/polls/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+from .models import user
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index")
+    myusers = user.objects.all().values()
+    template = loader.get_template('user_list.html')
+    context = {
+        'myusers': myusers,
+    }
+    return HttpResponse(template.render(context, request))
 
 
+from .forms import InputForm
+
+def home_view(request):
+    context = {}
+    context["form"] =  InputForm()
+    return render(request, "home.html",  context)
+
+
+
+def form_view(request):
+    if request.method ==  "POST":
+        print(request.POST)
+        name =  request.POST.get('your_name')
+    return render(request, "form.html")
