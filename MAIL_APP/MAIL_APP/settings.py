5a22407227b21e0319e2c08a6b4b192ed653e53c
Mannat kumar <cybertyist@gmail.com>
2026-02-17 09:47:35 +0530

    Disable email backend settings in settings.py
    Comment out email configuration settings for security.

--- MAIL_APP/MAIL_APP/settings.py
+++ MAIL_APP/MAIL_APP/settings.py
@@ -32,8 +32,7 @@ ALLOWED_HOSTS = []
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'mannatkumaroffical01@gmail.com'
-# EMAIL_HOST_PASSWORD = 'lkhtgipoasirtudy'
+
 
 
 # Application definition
